40a8ca0d5d34bc80f946d231b27de2a97f08c90b
pereiradaniel <danielpaulpereira@protonmail.com>
2025-01-24 14:18:18 -0500

    Updated gradient RGB comments.

--- arduino_sketches/chapter_5/gradient_RGB_light/gradient_RGB_light.ino
+++ arduino_sketches/chapter_5/gradient_RGB_light/gradient_RGB_light.ino
@@ -1,5 +1,7 @@
+// Objective: An RGB LED will be controlled through a gradient using values 0 to 255.
+
 // Defines RGB LED pins:
-const byte rgbPins[] = {4, 2, 15};   // GPIO pins for RGB LED
+const byte rgbPins[] = {4, 2, 15};   // GPIO pins on ESP32
 
 // Defines PWM channels:
 const byte chans[]   = {0, 1, 2};
@@ -11,18 +13,26 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 256; ++i)
-    {
-      setColour(wheel(i));
-      delay(20);
-    }
+    for (int i = 0; i < 256; ++i)
+        {
+            setColour(wheel(i));
+            delay(10);
+        }
 }
 
+// void setColour(long rgb):
+//      - rgb is a hexadecimal representation of colour (see wheel()).
+
 void setColour(long rgb) {
+    // Reads the value of each colour channel:
     ledcWrite(rgbPins[0], 255 - (rgb >> 16) & 0xFF);
     ledcWrite(rgbPins[1], 255 - (rgb >>  8) & 0xFF);
     ledcWrite(rgbPins[2], 255 - (rgb >>  0) & 0xFF);
 }
+
+// long wheel():
+//      - is the colour selection method, and takes int pos which is a value from 0 to 255.
+//      - Returns a colour value in hexadecimal.
 
 long wheel(int pos) {
     long WheelPos = pos % 0xff;

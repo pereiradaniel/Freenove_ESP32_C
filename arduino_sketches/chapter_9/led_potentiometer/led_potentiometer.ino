9476410c278acb2dee99633759e49d1fcf6d99f7
pereiradaniel <danielpaulpereira@protonmail.com>
2025-01-28 16:11:32 -0500

    Finished chapter 9

--- arduino_sketches/chapter_9/led_potentiometer/led_potentiometer.ino
+++ arduino_sketches/chapter_9/led_potentiometer/led_potentiometer.ino
@@ -7,14 +7,19 @@ void setup() {
 
 void loop() {
   // Obtain ADC value using analogRead():
+  // analogRead() reads the value from the specified analog pin.
+  // Return the analog reading on the pin.
+  // (0-4095 for 12 bits)
   int adcVal = analogRead(PIN_ANALOG_IN);
 
   // Convert ADC vaue into 8-bit precision DAC value using map():
+  // map() re-maps a number from one range to another.
   int dacVal = map(adcVal, 0, 4095, 0, 255);
 
   double voltage = adcVal / 4095.0 * 3.3;
   
   // Output the value using dacWrite();
+  // dacWrite() writes the given value to the supplied analog pin.
   dacWrite(DAC1, dacVal);
 
   // Print message to the serial console:

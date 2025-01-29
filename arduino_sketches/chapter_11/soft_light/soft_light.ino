c47975fc2f17a057769d851fa1de2bd8bb0df612
pereiradaniel <danielpaulpereira@protonmail.com>
2025-01-29 14:12:16 -0500

    Started soft_light.ino

---
+++ arduino_sketches/chapter_11/soft_light/soft_light.ino
@@ -0,0 +1,31 @@
+#define PIN_ANALOG_IN 4
+#define PIN_LED       25
+#define CHAN          0
+
+void setup() {
+    // Setup LEDC pin with frequency, resolution, and channel:
+    //      LEDC pin:                       PIN_LED (GPIO 25)
+    //      Frequency of PWM:               1000
+    //      Resolution for LEDC channel:    12 bits
+    //      Channel:                        CHAN (0)
+    ledcAttachChannel(PIN_LED, 1000, 12, CHAN);
+}
+
+void loop() {
+    // analogRead():
+    //      Gets ADC raw value for a given pin/ADC channel.
+    int adc = analogRead(PIN_ANALOG_IN);
+
+    // Re-map adc to pwm:
+    int pwm = adc;
+
+    // Set the LEDC duty to the pulse width of pwm:
+    // ledcWrite():
+    //      Used to set duty for the LEDC pin.
+    //      LEDC pin:   PIN_LED (GPIO 25)
+    //      LEDC duty:  pwm (read from PIN_ANALOG_IN (GPIO 4))
+    ledcWrite(PIN_LED, pwm);
+
+    // Add some delay:
+    delay(10);
+}

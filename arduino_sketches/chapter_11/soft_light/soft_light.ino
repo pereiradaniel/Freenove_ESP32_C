f09aea0c2eeb7f45813da0addbdb3d50448bf863
pereiradaniel <danielpaulpereira@protonmail.com>
2025-01-29 14:34:02 -0500

    Finished: soft_light.ino

--- arduino_sketches/chapter_11/soft_light/soft_light.ino
+++ arduino_sketches/chapter_11/soft_light/soft_light.ino
@@ -3,12 +3,17 @@
 #define CHAN          0
 
 void setup() {
+    Serial.begin(115200);
+
     // Setup LEDC pin with frequency, resolution, and channel:
     //      LEDC pin:                       PIN_LED (GPIO 25)
     //      Frequency of PWM:               1000
     //      Resolution for LEDC channel:    12 bits
     //      Channel:                        CHAN (0)
     ledcAttachChannel(PIN_LED, 1000, 12, CHAN);
+
+    // Print a message to the console with initialilzation data:
+    Serial.printf("Analog in: %d\nLED pin: %d\nChannel: %d\n", PIN_ANALOG_IN, PIN_LED, CHAN);
 }
 
 void loop() {
@@ -25,7 +30,10 @@ void loop() {
     //      LEDC pin:   PIN_LED (GPIO 25)
     //      LEDC duty:  pwm (read from PIN_ANALOG_IN (GPIO 4))
     ledcWrite(PIN_LED, pwm);
-
+    
+    // Print a message to the console:
+    Serial.printf("PWM/LEDC duty: %d\n", pwm);
+    
     // Add some delay:
     delay(10);
 }

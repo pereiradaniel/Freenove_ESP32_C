afa82e04d3bb62b4ff7bfcf684997be3bc068166
pereiradaniel <danielpaulpereira@protonmail.com>
2025-01-29 10:44:20 -0500

    Finished: touch_sensor.ino, added explanatory comments.

--- arduino_sketches/chapter_10/touch_sensor/touch_sensor.ino
+++ arduino_sketches/chapter_10/touch_sensor/touch_sensor.ino
@@ -7,6 +7,9 @@ void setup() {
 
 void loop() {
     // Prints the touch value to the serial monitor:
+    // touchRead():
+    //  - Reads the touch sensor value at specified pin. 
+    //  - A value close to 0 means that a touch has been detected.
     Serial.printf("Touch value: %d \n", touchRead(T0));
     delay(1000);
 }

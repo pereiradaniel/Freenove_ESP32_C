cdd774bdac380e620235a86f1f487e62e124a3c1
pereiradaniel <danielpaulpereira@protonmail.com>
2025-02-04 14:16:43 -0500

    Started servo_sweep.ino

---
+++ arduino_sketches/chapter_18/Servo_sweep/servo_sweep.ino
@@ -0,0 +1,43 @@
+// ESP32Servo library is required in this project:
+#include <ESP32Servo.h>
+
+// Create a Servo class object:
+Servo myservo;
+
+// Initialize variables:
+int posVal = 0;  // servo position
+int servoPin = 15;  // servo motor pin
+
+void setup() {
+    // Standard 50 hz servo:
+    myservo.setPeriodHertz(50);
+  
+    // Attach pin to the Servo object:
+    // myservo.attach(): arguments for motor pin and time range
+    myservo.attach(servoPin, 500, 2500);
+}
+
+void loop() {
+    // MOVEMENT:
+    //  Movement infinitely loops, and takes place in two phases:
+    //      Phase 1:
+    //          - Servo arm moves from 0 to 180 degrees.
+    //      Phase 2:
+    //          - Servo arm moves from 180 to 0 degrees.
+
+    // Phase 1:
+    for (posVal = 0; posVal <= 180; posVal += 1) {
+        // Servo goes to position posVal:
+        myservo.write(posVal);
+        // Wait 15 ms for servo to reach end position:
+        delay(15);
+    }
+
+    // Phase 2:
+    for (posVal = 180; posVal >= 0; posVal -= 1) { 
+        // Servo goes to position posVal:
+        myservo.write(posVal);
+        // Wait 15 ms for servo to reach end position:
+        delay(15);
+    }
+}

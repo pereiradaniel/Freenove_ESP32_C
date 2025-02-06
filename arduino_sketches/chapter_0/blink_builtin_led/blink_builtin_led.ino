d84cb4ce2466a6689bc0629a98af1395afdf6dd5
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-02-06 14:51:18 -0500

    Begin work on Project 22.2

--- arduino_sketches/chapter_0/blink_builtin_led/blink_builtin_led.ino
+++ arduino_sketches/chapter_0/blink_builtin_led/blink_builtin_led.ino
@@ -1,22 +1,22 @@
 // Set BOARD_LED to GPIO pin 2, which is the pin for the builtin LED on the ESP32 board.
-# define BOARD_LED 2
+#define GPIO_2 2
 
 void setup()
 {
-        // Initializes BOARD_LED as an OUTPUT so that it can be set to HIGH or LOW.
-        pinMode(BOARD_LED, OUTPUT);
+        // Initializes GPIO_2 as an OUTPUT so that it can be set to HIGH or LOW.
+        pinMode(GPIO_2, OUTPUT);
 }
 
 void loop()
 {
     // Setting GPIO pin 2 to HIGH turns the LED OFF:
-    digitalWrite(BOARD_LED, HIGH);
+    digitalWrite(GPIO_2, HIGH);
     
     // Delay in milliseconds:
     delay(500);
 
     // Setting GPIO pin 2 to LOW tuns the LED ON:
-    digitalWrite(BOARD_LED, LOW);
+    digitalWrite(GPIO_2, LOW);
 
     // Delay in milliseconds:
     delay(500);

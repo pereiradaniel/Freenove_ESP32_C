dcc34c43b587da1953c0691d70dfdf22dde97669
Daniel Pereira <danielpaulpereira@protonmail.com>
2025-02-10 15:16:20 -0500

    Added ir_remote.ino

---
+++ arduino_sketches/chapter_23/ir_remote/ir_remote.ino
@@ -0,0 +1,29 @@
+#include "Freenove_IR_Lib_for_ESP32.h"
+
+// GPIO Pin 15 is connected to IR reciever:
+const uint16_t recvPin = 15;
+
+// Create IR receiver class object:
+Freenove_ESP32_IR_Recv ir_recv(recvPin);
+
+void setup() {
+    // Initialize ESP32 with serial connection:
+    Serial.begin(115200);
+  
+    // Send message to terminal to notify when IR receiver is waiting for a signal from the remote:
+    Serial.print("IR receiver is waiting for IR message. ");
+  
+    // Print the IR receiver pin output to the serial monitor:
+    Serial.println(recvPin);
+}
+
+void loop() {
+    // Gets IR received data:
+    ir_recv.task();
+  
+    // Check NEC protocol:
+    if(ir_recv.nec_available()){
+      // Prints the code received from the IR remote:
+      Serial.printf("IR Code:  %#x\r\n", ir_recv.data());
+    }
+}

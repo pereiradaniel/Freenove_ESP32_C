1d7cf543630044cb02e8d1879112e533a245dc5f
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2025-02-18 17:03:40 -0500

    Edited project 32.1, do not include WiFi name and password.

--- arduino_sketches/chapter_32/wifi_station/wifi_station.ino
+++ arduino_sketches/chapter_32/wifi_station/wifi_station.ino
@@ -1,22 +1,40 @@
+// Project 32.1: WiFi Station.
+// This project sets up an ESP32 in WiFi station mode.
+
 #include <WiFi.h>
-  
-// const char *ssid_Router     = "********"; //Enter the router name
-// const char *password_Router = "********"; //Enter the router password
+
+// WiFi Router and password variables:  
+const char *ssid_Router     = "********"; //Enter the router name
+const char *password_Router = "********"; //Enter the router password
 
 void setup(){
+    // Initialize serial connection with ESP32 via USB:
     Serial.begin(115200);
+
+    / /Add some delay:
     delay(2000);
+
+    // Print a message to the console notifying 
     Serial.println("Setup start");
+
+    // Initialize WIFI connection:
     WiFi.begin(ssid_Router, password_Router);
     Serial.println(String("Connecting to ")+ssid_Router);
-    while (WiFi.status() != WL_CONNECTED){
+    
+    // Display counter as connection is processed:
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
   
-    Serial.println("\nConnected, IP address: ");
+    // After WIFI connection is established:
+
+    // Display connection message and IP address: 
+    Serial.println("\nConnected with IP address: ");
     Serial.println(WiFi.localIP());
-    Serial.println("Setup End");
+
+    // Display completion message: 
+    Serial.println("WiFi setup completed.");
 }
  
 void loop() {
